e4992fd81918993465717a174794b89587b38f14
Cauê Josê Oliveira Falcão <cauejose007@gmail.com>
2024-03-07 09:29:04 -0400

    arquivos

--- oi.py
+++ oi.py
@@ -1,9 +1,18 @@
-x = 3
-while True:
-    try:
-        print(10/x)
-        x -= 1
-    except ZeroDivisionError:
-        print('dividiu por zero')
-        break
 
+
+arquivo = open('text.txt', 'r')
+numeroAlunos = 0
+numeroReprova = 0
+somaNotas = 0
+media = 0
+for linha in arquivo:
+    numeroAlunos += 1
+    nome = linha.split()[0]
+    nota = float(linha.split()[1])
+    if nota < 7.0:
+        numeroReprova += 1
+    somaNotas += nota
+media = somaNotas / numeroAlunos
+
+print(media, numeroAlunos, numeroReprova)
+arquivo.close()
